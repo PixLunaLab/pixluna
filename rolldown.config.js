889f179d02d0df2a3ec077a6dd3e24680142a4ab
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2025-08-29 14:22:01 +0800

    chore: 更换打包器

---
+++ rolldown.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'rolldown'
+import dependencies from './package.json' with { type: 'json' }
+import { dts } from 'rolldown-plugin-dts'
+
+const external = new RegExp(
+  `^(node:|${[...Object.getOwnPropertyNames(dependencies.devDependencies ? dependencies.devDependencies : []), ...Object.getOwnPropertyNames(dependencies.dependencies ? dependencies.dependencies : [])].join('|')})`
+)
+
+const config = {
+  input: './src/index.ts'
+}
+
+export default defineConfig([
+  {
+    ...config,
+    output: [{ file: 'lib/index.mjs', format: 'es', minify: true }],
+    external: external
+  },
+  {
+    ...config,
+    output: [{ file: 'lib/index.cjs', format: 'cjs', minify: true }],
+    external: external
+  },
+  {
+    ...config,
+    output: [{ dir: 'lib', format: 'es' }],
+    plugins: [dts({ emitDtsOnly: true })],
+    external: external
+  }
+])
